ca1619d8091b5c0cd6307ff5538f0b08e7035fe4
Herlin Priatna <104877712+herlinpriatna@users.noreply.github.com>
2022-12-26 16:32:46 +0700

    Update lobby.cpp

--- lobby.cpp
+++ lobby.cpp
@@ -22,13 +22,40 @@ void removePlayer(list_Lobby &L, ){
 }
 
 void deleteLobby(list_Lobby &L, adrLobby &P);{
-    
+    adrLobby Q;
+    if(first(L) == NULL){
+        cout << "Lobby Kosong" << endl;
+    } else if (next(first(L))==NULL){
+        P = first(L);
+        first(L) = NULL;
+    } else {
+        Q = first(L);
+        while(next(Q)!=NULL){
+            Q = next(Q);   
+        }
+        first(L) = nextLobby(P);
+        nextLobby(Q) = nextLobby(P);
+        nextLobby(P) = NULL;
+           
+    }
 }
 void showAllLobby(list_Lobby L){
-    
+    adrLobby P;
+    if(first(L)!=NULL){
+        P = first(L);
+        while(P!=NULL){
+            cout << "ID Lobby : " << infoLobby(P).id << endl;
+            cout << "Total Player : " << infoLobby(P).totalPlayer << endl;
+            cout << "Rank : " << infoLobby(P).rank << endl;
+            P = nextLobby(P);
+        }
+        cout << endl;
+    } else {
+        cout << "Lobby Kosong" << endl;
+    }
 }
 
-bool findLobby(list_Lobby L, infotype_lobby x){
+bool findLobby(list_Lobby L, int id){
     
 }
 
