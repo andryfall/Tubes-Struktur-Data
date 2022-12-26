470152fd8e1435f8a2d5ab629a979ec4cc52336e
Herlin Priatna <104877712+herlinpriatna@users.noreply.github.com>
2022-12-26 16:11:38 +0700

    Update lobby.cpp

--- lobby.cpp
+++ lobby.cpp
@@ -4,12 +4,34 @@ void createLobby(list_Lobby &L, infotype_lobby lobbyBaru){
     P = new elmLobby;
     infoLobby(P) = lobbyBaru;
     nextLobby(P) = NULL;
+    
+    if(first(L)==NULL){
+        first(L) = P;
+    } else {
+        adrLobby Q;
+        Q = first(L);
+        while(Q!=NULL){
+            Q = next(Q);
+        }
+        next(Q) = P;
+    }
 }
 
 void removePlayer(list_Lobby &L, ){
     
 }
-void deleteLobby(list_Lobby &L, adrLobby &P);
-void showAllLobby(list_Lobby L);
-bool findLobby(list_Lobby L, infotype_lobby x);
-void showLobbyData(list_Lobby L, adrLobby P);
+
+void deleteLobby(list_Lobby &L, adrLobby &P);{
+    
+}
+void showAllLobby(list_Lobby L){
+    
+}
+
+bool findLobby(list_Lobby L, infotype_lobby x){
+    
+}
+
+void showLobbyData(list_Lobby L, adrLobby P){
+    
+}
