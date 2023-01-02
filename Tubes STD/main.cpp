b2861003eb5a4f210e80a7e289b3c95d664fe02f
andryfall <91175702+andryfall@users.noreply.github.com>
2023-01-02 13:32:40 +0700

    Update main.cpp

--- Tubes STD/main.cpp
+++ Tubes STD/main.cpp
@@ -10,67 +10,119 @@ int main()
     list_player U;
     infotype_player x;
     adrPlayer p;
+    int pilihan = 0;
 
     //baru testing
     createListLobby(L);
     createListPlayer(U);
 
-    x.playerName = "Saya";
-    x.rankPlayer = "Bronze";
-    insertPlayer(U, newPlayer(x));
+    pilihan = selectMenu();
+    while(pilihan != 0){
+        switch(pilihan){
+        case 1:
+            {int jumlah;
+            string kembali;
+            int i = 0;
+            cout<<"Jumlah player yang akan ditambahkan: ";
+            cin >> jumlah;
+            while (i < jumlah) {
+                cout<<"Nama Player : ";
+                cin>>x.playerName;
+                cout<<"Rank Player : ";
+                cin>>x.rankPlayer;
 
-    x.playerName = "Dia";
-    x.rankPlayer = "Bronze";
-    insertPlayer(U, newPlayer(x));
+                p = newPlayer(x);
+                insertPlayer(U, p);
+                i++;
+            }
+            cout<<"Kembali ke menu utama? (Y/N) : ";
+            cin>>kembali;
+            if (kembali == "Y"){
+                pilihan = 0;
+            }
+            break;
+            }
+        case 2:
+            connectToLobby(L, U);
+            cout<<endl;
+            cout<<"===Matchmaking Finished==="<<endl;
+            cout<<endl;
+            break;
+        case 3:
+            {
+            showAvailableLobby(L);
+            break;
+            }
+        case 4:
+            {
+            showAllLobby(L, U);
+            break;
+            }
+        case 5:
+            {adrPlayer fP;
+            string nama;
+            cout<<endl;
+            cout<<"===========Find Player=========="<<endl;
+            cout<<"Nama player yang ingin di cari : ";
+            cin>>nama;
+            fP = searchPlayer(U, nama);
+            cout<<endl;
+            cout<<"===========Result=========="<<endl;
+            if (fP == NULL){
+                cout<<"Player tidak ditemukan"<<endl;
+            }else{
+                adrLobby t = lobby(fP);
+                cout<<"Hasil Pencarian : "<<endl;
+                cout<<"Nama Player : ";
+                cout<<infoPlayer(fP).playerName<<endl;
+                cout<<"Rank Player : ";
+                cout<<infoPlayer(fP).rankPlayer<<endl;
+                cout<<"Berada di lobby : ";
+                if(t == NULL){
+                    cout<<"-"<<endl;
+                }else{
+                    cout<<infoLobby(t).nomor<<endl;
+                }
+            }
+            cout<<endl;
+            break;
+            }
+        case 6:
+            {string fP;
+            int fL;
+            cout<<endl;
+            cout<<"===========Find Player In Lobby=========="<<endl;
+            cout<<"Nama player yang ingin di cari : ";
+            cin>>fP;
+            cout<<"Dicari pada lobby ke-";
+            cin>> fL;
 
-    x.playerName = "Kami";
-    x.rankPlayer = "Silver";
-    insertPlayer(U, newPlayer(x));
+            cout<<"===========Result=========="<<endl;
+            searchPlayerInLobby(L, U, fP, fL);
+            cout<<endl;
+            break;
+            }
+        case 7:
+            {string nama;
+            cout<<endl;
+            cout<<"===========Delete Player=========="<<endl;
+            cout<<"Nama player yang akan dihapus : ";
+            cin>>nama;
 
-
-
-    connectToLobby(L, U);
-
-    adrLobby n;
-    p = searchPlayer(U, "Saya");
-    n = lobby(p);
-    cout <<infoPlayer(p).playerName<<endl;
-    cout <<infoPlayer(p).rankPlayer<<endl;
-    cout <<infoLobby(n).nomor << endl;
-
-    p = searchPlayer(U, "Dia");
-    n = lobby(p);
-    cout <<infoPlayer(p).playerName<<endl;
-    cout <<infoPlayer(p).rankPlayer<<endl;
-    cout <<infoLobby(n).nomor << endl;
-
-    p = searchPlayer(U, "Kami");
-    n = lobby(p);
-    cout <<infoPlayer(p).playerName<<endl;
-    cout <<infoPlayer(p).rankPlayer<<endl;
-    cout <<infoLobby(n).nomor << endl;
-
-
-    cout<<"================================================="<<endl;
-    showAllLobby(L, U);
-    cout<<"================================================="<<endl;
-
-
-    adrPlayer dP;
-    dP = searchPlayer(U, "Dia");
-    deletePlayer(U, dP);
-
-    cout<<"================================================="<<endl;
-    showAllLobby(L, U);
-    cout<<"================================================="<<endl;
-
-    adrLobby dL;
-    dL = searchLobby(L, "Silver");
-    deleteLobby(L, dL);
-
-    cout<<"================================================="<<endl;
-    showAllLobby(L, U);
-    cout<<"================================================="<<endl;
-
-
+            adrPlayer fP = searchPlayer(U, nama);
+            deletePlayer(U, fP);
+            cout<<"==Delete Berhasil=="<<endl;
+            break;
+            }
+        case 8:
+            {
+                adrLobby s = searchLobby(L, "Silver");
+                int t = countPlayer(U, s);
+                cout<<t<<endl;
+            }
+        }
+        pilihan = selectMenu();
+    }
+    cout<<"ANDA TELAH KELUAR DARI PROGRAM"<<endl;
+    return 0;
 }
